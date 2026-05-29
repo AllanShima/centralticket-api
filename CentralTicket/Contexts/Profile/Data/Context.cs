ba9adbe2697d44fa05fd94a34a10cddaec948f00
Guilherme Mitsuzumi <mitsuzumiguilhermeryu@gmail.com>
2026-05-28 21:31:31 -0300

    Finalizando profile context

---
+++ CentralTicket/Contexts/Profile/Data/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CentralTicket.Contexts.Profile.Entities;
+namespace CentralTicket.Contexts.Profile.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sale>()
+                .HasKey(sale => sale.Id);
+
+            modelBuilder.Entity<User>()
+                .HasKey(user => user.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
